cc84093cd4fc098536834c719cd7921fdef74e7a
karamalmlahe <karamturi@hotmail.com>
2022-12-07 14:07:02 +0200

    aa

--- address/address.js
+++ address/address.js
@@ -2,5 +2,4 @@ const nextPage = () => {
     const cityName = document.getElementById("city").value
     const streetName = document.getElementById("street")
     const homeNumber = document.getElementById("home-number")
-  
 }
