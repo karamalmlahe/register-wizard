f399a4d7637890d4935ad850d90a96f9457c0add
Fardous <109430495+FardousAbuZaid@users.noreply.github.com>
2022-12-07 14:12:12 +0200

    added a script of navigation and summary, and back button

---
+++ summary/summary.js
@@ -0,0 +1,20 @@
+function display() {
+  //retrive all data from storege.js
+  fullName = getItembyKey("name");
+  email = getItembyKey("email");
+  birthDate = getItembyKey("birthDate");
+  city = getItembyKey("city");
+  street = getItembyKey("street");
+  addressNumber = getItembyKey("number");
+  img = getItembyKey("img");
+  hobbies = getItembyKey("hobbies");
+
+  //showing data
+  document.getElementById("name").innerHTML = "Hello, " + fullName;
+  document.getElementById("address").innerHTML =
+    city + ", " + street + ", " + addressNumber;
+  document.getElementById("email").innerHTML = email;
+  document.getElementById("birthDate").innerHTML = birthDate;
+  document.getElementById("hobbies").innerHTML = hobbies;
+  document.getElementById("img").src = img;
+}
