d59c4dc8a027a1a54f32dd6e5f292bd544a13918
AhmadMdegem <103180487+AhmadMdegem@users.noreply.github.com>
2022-12-07 13:23:47 +0200

    +

--- address/address.js
+++ address/address.js
@@ -3,7 +3,9 @@ const nextPage = () => {
     const streetName = document.getElementById("street")
     const homeNumber = document.getElementById("home-number")
     const MAXLENGTH = 20;
-    if (required(cityName) && required(streetName)) {
+    console.log(required('sadhk'));
+    if (!required(cityName) && !required(streetName)) {
+        console.log('karam');
         if (maxLength(cityName, MAXLENGTH) && maxLength(streetName, MAXLENGTH))
             addToLocalStorege(cityName, streetName, homeNumber)
             getNextPage()
