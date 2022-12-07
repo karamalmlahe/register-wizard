69296bcebd6c786b6ed597a872b1e8b03cf109f7
karamalmlahe <karamturi@hotmail.com>
2022-12-07 10:55:42 +0200

    added a empty funtions,css api (navigation,css,storege,validation)

---
+++ navigation.js
@@ -0,0 +1,3 @@
+const getNextPage = () => {};
+const getCurrentPage = () => {};
+const getPreviousPage = () => {};
---
+++ validation.js
@@ -0,0 +1,25 @@
+const required = (value) => {
+  return true;
+};
+
+const minLength = (value, minLength) => {
+  return true;
+};
+const maxLength = (value, maxLength) => {
+  return true;
+};
+const validEmail = (value) => {
+  return true;
+};
+const validPhoneNumber = (value) => {
+  return true;
+};
+const validUrlPhoto = (value) => {
+  return true;
+};
+const validFullName = (value) => {
+  return true;
+};
+const validBirthDate = (value) => {
+  return true;
+};
