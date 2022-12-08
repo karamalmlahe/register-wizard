2f602ac877da97bdbbf645043c8c31c61a6e1bc4
alshafaeeahmed <alshafaeeahmed@gmail.com>
2022-12-08 13:50:56 +0200

    adding error messages for personal details validation

--- personal-details/personal_details.js
+++ personal-details/personal_details.js
@@ -1,9 +1,11 @@
 const next = () => {
-    const fullName = document.getElementById("full_name").value
+    const fullName = document.getElementById("fullName").value
     const email = document.getElementById("email").value
-    const birthDate = document.getElementById("birth_date").value
-    if (validPersonalDetails(fullName, email, birthDate))
+    const birthDate = document.getElementById("birthDate").value
+    if (validPersonalDetails(fullName, email, birthDate)) {
+        addToLocalStorege(fullName, email, birthDate)
         getNextPage();
+    }
 
 
 
@@ -11,27 +13,37 @@ const next = () => {
 }
 const validPersonalDetails = (fullName, email, birthDate) => {
     const MAXLENGTH = 20;
-    if (!required(fullName) || !maxLength(fullName, maxLength)) {
-        document.getElementById("fullName-error").innerText = "error";
+    if (!required(fullName)) {
+        document.getElementById("fullName-error").innerText = 'Full name is required'
+        return false
+    } else if (!maxLength(fullName, MAXLENGTH)) {
+        document.getElementById("fullName-error").innerText = `error, max length is ${MAXLENGTH}`
+        return false
+    }
+    if (!required(email)) {
+        document.getElementById("email-error").innerText = 'Email is required'
+        return false
+    } else if (!validEmail(email)) {
+        document.getElementById("email-error").innerText = 'Email is not valid'
+        return false
+    }
+    if (!required(birthDate)) {
+        document.getElementById("birthDate-error").innerText = 'Birth Date is required'
+        return false
+    } else if (!validBirthDate(birthDate)) {
+        document.getElementById("birthDate-error").innerText = 'Birth Date is not valid';
         return false;
     }
-    if (!required(email) || !validEmail(email)) {
-        document.getElementById("email-error").innerText = "error";
-        return false;
-    }
-    if (!required(birthDate) || !validBirthDate(birthDate)) {
-        document.getElementById("birthDate-error").innerText = "error";
-        return false;
-    }
-
-    if (!validFullName(fullName))
-        return false;
-    else
-        validEmail(email);
-    validBirthDate(birthDate);
-
-
+    return true
 }
+
+const addToLocalStorege = (fullName, email, birthDate) => {
+    addItem("fullName", fullName);
+    addItem("email", email);
+    addItem("birthDate", birthDate);
+};
+
+
 
 
 
