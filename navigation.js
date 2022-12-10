f35136197a69c307a3aec9f96abc9ee3ca041113
AhmadMdegem <103180487+AhmadMdegem@users.noreply.github.com>
2022-12-10 22:18:12 +0200

    add a new phase splash-screen

--- navigation.js
+++ navigation.js
@@ -1,21 +1,22 @@
 const pagesPath = {
-  personal_details: ".././personal-details/personal_details.html",
-  address: ".././address/address.html",
-  miscellaneous: ".././miscellaneous/miscellaneous.html",
-  summary: ".././summary/summary.html"
+    splash_screen: "../register-wizard/splash-screen/splash-screen.html",
+    personal_details: ".././personal-details/personal_details.html",
+    address: ".././address/address.html",
+    miscellaneous: ".././miscellaneous/miscellaneous.html",
+    summary: ".././summary/summary.html"
 };
-const pagesPathKeys =Object.keys(pagesPath);
+const pagesPathKeys = Object.keys(pagesPath);
 
 const getNextPage = (currentPage) => {
-    const indexofNextPage = pagesPathKeys.indexOf(currentPage)+1;
-    if(indexofNextPage<pagesPathKeys.length){
-        window.location.href=pagesPath[pagesPathKeys[indexofNextPage]];
+    const indexofNextPage = pagesPathKeys.indexOf(currentPage) + 1;
+    if (indexofNextPage < pagesPathKeys.length) {
+        window.location.href = pagesPath[pagesPathKeys[indexofNextPage]];
     }
 };
 const getPreviousPage = (currentPage) => {
-    const indexofPreviousPage = pagesPathKeys.indexOf(currentPage)-1;
-    if(indexofPreviousPage>=0){
-        window.location.href=pagesPath[pagesPathKeys[indexofPreviousPage]];
+    const indexofPreviousPage = pagesPathKeys.indexOf(currentPage) - 1;
+    if (indexofPreviousPage >= 0) {
+        window.location.href = pagesPath[pagesPathKeys[indexofPreviousPage]];
     }
-    
+
 };
